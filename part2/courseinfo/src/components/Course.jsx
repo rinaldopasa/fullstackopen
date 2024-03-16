62fe97fee45dd40391e49d6dd1e54c18ca2d6d95
Rinaldo Pasa <62827386+rinaldopasa@users.noreply.github.com>
2024-03-16 12:01:31 +0800

    Fix: Applied the rules

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,28 @@
-import Content from "./Content";
-import Header from "./Header";
-import Total from "./Total";
+const Header = ({ title }) => <h2>{title}</h2>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  );
+};
+
+const TotalExercises = ({ total }) => {
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  );
+};
 
 const Course = ({ course }) => {
   const total = course.parts.reduce((s, p) => s + p.exercises, 0);
@@ -9,7 +31,7 @@ const Course = ({ course }) => {
     <div>
       <Header title={course.name} />
       <Content parts={course.parts} />
-      <Total total={total} />
+      <TotalExercises total={total} />
     </div>
   );
 };
--- part2/courseinfo/src/components/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({ total }) => {
-  return (
-    <p>
-      <strong>total of {total} exercises</strong>
-    </p>
-  );
-};
-
-export default Total;
